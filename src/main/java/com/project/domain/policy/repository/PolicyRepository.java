fd53796953b306290cb937327117cf581c434395
yechan <100present@naver.com>
2026-03-03 14:52:19 +0900

    DABOM-256 remove : admin, policy 코드 삭제

--- src/main/java/com/project/domain/policy/repository/PolicyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.domain.policy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.domain.policy.entity.Policy;
-
-public interface PolicyRepository extends JpaRepository<Policy, Long> {}
