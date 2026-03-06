7a76af28ca9193aa4571fa1342fd68a808adeca2
swthewhite <swthewhite@gmail.com>
2026-03-06 17:30:07 +0900

    DABOM-371 refactor: JsonDeserializer를 사용하지 않으므로 제거

--- src/main/java/com/project/global/config/KafkaConfig.java
+++ src/main/java/com/project/global/config/KafkaConfig.java
@@ -80,7 +80,6 @@ public class KafkaConfig {
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, StringDeserializer.class);
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.project.*");
         return config;
     }
 
