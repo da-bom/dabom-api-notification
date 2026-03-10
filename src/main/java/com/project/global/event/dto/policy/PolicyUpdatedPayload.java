24721aa6af3ba22d0cee1f8703c51050ed18338d
ChoiSeungeon <chygold06@gmail.com>
2026-03-10 21:57:42 +0900

    DABOM-400 remove: global Kafka 공통 DTO/설정 제거

--- src/main/java/com/project/global/event/dto/policy/PolicyUpdatedPayload.java
+++
@@ -1,9 +0,0 @@
-package com.project.global.event.dto.policy;
-
-public record PolicyUpdatedPayload(
-        Long familyId,
-        Long targetCustomerId,
-        String policyKey,
-        String oldValue,
-        String newValue,
-        Long changedBy) {}
