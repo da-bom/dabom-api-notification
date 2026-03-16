2d64b8cfb90926c90c4f318061ab83244e57dd26
swthewhite <swthewhite@gmail.com>
2026-03-16 18:17:08 +0900

    DABOM-302 refactor: EventStreamController를 event 도메인 패키지로 이동
    - family/controller/ → event/controller/ 패키지 이동
    - SSE 이벤트 스트림은 family 도메인이 아닌 범용 기능이므로
      독립 도메인 패키지로 분리

--- src/main/java/com/project/domain/event/controller/EventStreamController.java
+++ src/main/java/com/project/domain/event/controller/EventStreamController.java
@@ -1,4 +1,4 @@
-package com.project.domain.family.controller;
+package com.project.domain.event.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
